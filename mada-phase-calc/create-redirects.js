c85501abc39f790755069eb127cafb6629248c32
KorthaseC <c.korthase92@web.de>
2024-06-07 19:40:26 +0200

    change redirects

--- mada-phase-calc/create-redirects.js
+++ mada-phase-calc/create-redirects.js
@@ -2,7 +2,12 @@ const fs = require("fs");
 const path = require("path");
 
 const redirectsContent = "/*    /index.html   200\n";
-const redirectsPath = path.join(__dirname, "dist", "_redirects");
+const redirectsPath = path.join(
+  __dirname,
+  "dist",
+  "mada-phase-calc",
+  "_redirects"
+);
 
 fs.writeFileSync(redirectsPath, redirectsContent, "utf8");
 console.log("_redirects file created successfully");
